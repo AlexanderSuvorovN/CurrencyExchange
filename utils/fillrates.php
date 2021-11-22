246eb0a9d999e2e8e6eb0158e1b7c8ce01c84fa5
AlexanderSuvorovN <38633357+AlexanderSuvorovN@users.noreply.github.com>
2021-11-22 01:42:10 +0100

    Add files via upload

---
+++ utils/fillrates.php
@@ -0,0 +1,199 @@
+<?php
+	//
+	// USD / EUR
+	// https://www.exchangerates.org.uk/EUR-USD-exchange-rate-history.html
+	// https://www.exchangerates.org.uk/USD-EUR-exchange-rate-history.html
+	// 
+	// RUB / EUR
+	// https://www.exchangerates.org.uk/EUR-RUB-exchange-rate-history.html
+	// https://www.exchangerates.org.uk/RUB-EUR-exchange-rate-history.html
+	//
+	$rates = [];
+	/* */
+	$rates[] = ['2021-04-23', 'USD', 'EKO', 1.0000, 1.2099, 0.82650];
+	$rates[] = ['2021-04-22', 'USD', 'EKO', 1.0000, 1.2017, 0.83218];
+	$rates[] = ['2021-04-21', 'USD', 'EKO', 1.0000, 1.2035, 0.83092];
+	$rates[] = ['2021-04-20', 'USD', 'EKO', 1.0000, 1.2034, 0.83098];
+	$rates[] = ['2021-04-19', 'USD', 'EKO', 1.0000, 1.2038, 0.83071];
+	$rates[] = ['2021-04-18', 'USD', 'EKO', 1.0000, 1.1972, 0.83525];
+	$rates[] = ['2021-04-17', 'USD', 'EKO', 1.0000, 1.1982, 0.83460];
+	$rates[] = ['2021-04-16', 'USD', 'EKO', 1.0000, 1.1982, 0.83459];
+	$rates[] = ['2021-04-15', 'USD', 'EKO', 1.0000, 1.1968, 0.83558];
+	$rates[] = ['2021-04-14', 'USD', 'EKO', 1.0000, 1.1980, 0.83470];
+	$rates[] = ['2021-04-13', 'USD', 'EKO', 1.0000, 1.1950, 0.83684];
+	$rates[] = ['2021-04-12', 'USD', 'EKO', 1.0000, 1.1913, 0.83944];
+	$rates[] = ['2021-04-11', 'USD', 'EKO', 1.0000, 1.1899, 0.84042];
+	$rates[] = ['2021-04-10', 'USD', 'EKO', 1.0000, 1.1901, 0.84029];
+	$rates[] = ['2021-04-09', 'USD', 'EKO', 1.0000, 1.1901, 0.84025];
+	$rates[] = ['2021-04-08', 'USD', 'EKO', 1.0000, 1.1916, 0.83924];
+	$rates[] = ['2021-04-07', 'USD', 'EKO', 1.0000, 1.1871, 0.84237];
+	$rates[] = ['2021-04-06', 'USD', 'EKO', 1.0000, 1.1872, 0.84235];
+	$rates[] = ['2021-04-05', 'USD', 'EKO', 1.0000, 1.1815, 0.84641];
+	$rates[] = ['2021-04-04', 'USD', 'EKO', 1.0000, 1.1763, 0.85012];
+	$rates[] = ['2021-04-03', 'USD', 'EKO', 1.0000, 1.1759, 0.85040];
+	$rates[] = ['2021-04-02', 'USD', 'EKO', 1.0000, 1.1760, 0.85033];
+	$rates[] = ['2021-04-01', 'USD', 'EKO', 1.0000, 1.1780, 0.84888];
+	$rates[] = ['2021-03-31', 'USD', 'EKO', 1.0000, 1.1726, 0.85280];
+	$rates[] = ['2021-03-30', 'USD', 'EKO', 1.0000, 1.1719, 0.85335];
+	$rates[] = ['2021-03-29', 'USD', 'EKO', 1.0000, 1.1769, 0.84970];
+	$rates[] = ['2021-03-28', 'USD', 'EKO', 1.0000, 1.1791, 0.84812];
+
+	$rates[] = ['2021-04-23', 'RUB', 'EKO', 1.0000, 90.587, 0.011039];
+	$rates[] = ['2021-04-22', 'RUB', 'EKO', 1.0000, 90.704, 0.011025];
+	$rates[] = ['2021-04-21', 'RUB', 'EKO', 1.0000, 92.104, 0.010857];
+	$rates[] = ['2021-04-20', 'RUB', 'EKO', 1.0000, 92.924, 0.010762];
+	$rates[] = ['2021-04-19', 'RUB', 'EKO', 1.0000, 92.007, 0.010869];
+	$rates[] = ['2021-04-18', 'RUB', 'EKO', 1.0000, 90.661, 0.011030];
+	$rates[] = ['2021-04-17', 'RUB', 'EKO', 1.0000, 90.596, 0.011038];
+	$rates[] = ['2021-04-16', 'RUB', 'EKO', 1.0000, 90.596, 0.011038];
+	$rates[] = ['2021-04-15', 'RUB', 'EKO', 1.0000, 91.078, 0.010980];
+	$rates[] = ['2021-04-14', 'RUB', 'EKO', 1.0000, 90.914, 0.010999];
+	$rates[] = ['2021-04-13', 'RUB', 'EKO', 1.0000, 90.745, 0.011020];
+	$rates[] = ['2021-04-12', 'RUB', 'EKO', 1.0000, 92.113, 0.010856];
+	$rates[] = ['2021-04-11', 'RUB', 'EKO', 1.0000, 92.130, 0.010854];
+	$rates[] = ['2021-04-10', 'RUB', 'EKO', 1.0000, 92.126, 0.010855];
+	$rates[] = ['2021-04-09', 'RUB', 'EKO', 1.0000, 92.142, 0.010853];
+	$rates[] = ['2021-04-08', 'RUB', 'EKO', 1.0000, 91.522, 0.010926];
+	$rates[] = ['2021-04-07', 'RUB', 'EKO', 1.0000, 91.528, 0.010926];
+	$rates[] = ['2021-04-06', 'RUB', 'EKO', 1.0000, 91.642, 0.010912];
+	$rates[] = ['2021-04-05', 'RUB', 'EKO', 1.0000, 90.348, 0.011068];
+	$rates[] = ['2021-04-04', 'RUB', 'EKO', 1.0000, 89.932, 0.011120];
+	$rates[] = ['2021-04-03', 'RUB', 'EKO', 1.0000, 89.886, 0.011125];
+	$rates[] = ['2021-04-02', 'RUB', 'EKO', 1.0000, 89.890, 0.011125];
+	$rates[] = ['2021-04-01', 'RUB', 'EKO', 1.0000, 89.866, 0.011128];
+	$rates[] = ['2021-03-31', 'RUB', 'EKO', 1.0000, 88.735, 0.011269];
+	$rates[] = ['2021-03-30', 'RUB', 'EKO', 1.0000, 88.951, 0.011242];
+	$rates[] = ['2021-03-29', 'RUB', 'EKO', 1.0000, 89.072, 0.011227];
+	$rates[] = ['2021-03-28', 'RUB', 'EKO', 1.0000, 89.444, 0.011180];
+
+	$rates[] = ['2021-04-23', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-22', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-21', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-20', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-19', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-18', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-17', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-16', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-15', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-14', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-13', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-12', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-11', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-10', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-09', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-08', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-07', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-06', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-05', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-04', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-03', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-02', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-04-01', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-03-31', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-03-30', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-03-29', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	$rates[] = ['2021-03-28', 'EUR', 'EKO', 1.0000, 1.0000, 1.0000];
+	/* */
+	define('IX_DATE', 0);
+	define('IX_FROM', 1);
+	define('IX_TO', 2);
+	define('IX_UNIT', 3);
+	define('IX_FORWARD_RATE', 4);
+	define('IX_REVERSE_RATE', 5);
+	try
+	{
+		$database = 'ekonom';
+		$username = 'ekonom';
+		$password = 'bV7vQ3yO0fwB7l';
+		$dbh = new PDO("mysql:host=localhost;dbname=".$database.";charset=utf8", $username, $password);
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOException $e)
+	{
+		echo 'Can not establish database connection: '.$e->getMessage();
+	}
+	try
+	{
+		foreach($rates as $r)
+		{
+			$date = $r[IX_DATE];
+			$x_from = $r[IX_FROM];
+			$x_to = $r[IX_TO];
+			$unit = $r[IX_UNIT];
+			$forward_rate = $r[IX_FORWARD_RATE];
+			$reverse_rate = $r[IX_REVERSE_RATE];
+			$s = $dbh->prepare("INSERT IGNORE INTO `fx_rates` (`date`, `x_from`, `x_to`, `unit`, `forward_rate`, `reverse_rate`) VALUES(:date, :x_from, :x_to, :unit, :forward_rate, :reverse_rate)");
+			$s->bindParam(':date', $date, PDO::PARAM_STR);
+			$s->bindParam(':x_from', $x_from, PDO::PARAM_STR);
+			$s->bindParam(':x_to', $x_to, PDO::PARAM_STR);
+			$s->bindParam(':unit', $unit, PDO::PARAM_STR);
+			$s->bindParam(':forward_rate', $forward_rate, PDO::PARAM_STR);
+			$s->bindParam(':reverse_rate', $reverse_rate, PDO::PARAM_STR);
+			$s->execute();
+		}
+		$s = $dbh->prepare("SELECT `date`, `x_from`, `x_to`, `unit`, `forward_rate`, `reverse_rate` FROM `fx_rates` ORDER BY `date` DESC");
+		$s->execute();
+		$fetch = $s->fetchAll(PDO::FETCH_ASSOC);
+		$rates = $fetch;
+	}
+	catch(PDOException $e)
+	{
+		echo $e->getMessage();
+	}
+	$dbh = null;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+	body
+	{
+		font-family: 'Verdana', 'Tahoma', sans-serif;
+		font-size: 14px;
+		margin: 0;
+		padding: 0;
+	}
+	table
+	{
+		border-collapse: collapse;
+	}
+	th
+	{
+		text-align: right;
+		border-bottom: 2px solid black;
+		padding: 8px 16px;
+		background-color: #ccc;
+	}
+	td
+	{
+		text-align: right;
+		border-bottom: 1px solid #444;
+		padding: 8px 16px;
+	}
+	</style>
+</head>
+<body>
+	<table>
+		<thead>
+			<th>date</th>
+			<th>x_from</th>
+			<th>x_to</th>
+			<th>unit</th>
+			<th>forward_rate</th>
+			<th>reverse_rate</th>
+		</thead>
+		<tbody>
+			<?php foreach($rates as $r): ?>
+				<tr>
+					<td><?= $r['date'] ?></td>
+					<td><?= $r['x_from'] ?></td>
+					<td><?= $r['x_to'] ?></td>
+					<td><?= $r['unit'] ?></td>
+					<td><?= $r['forward_rate'] ?></td>
+					<td><?= $r['reverse_rate'] ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+</body>
+</html>
---
+++ utils/newsticker.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ЭКОНОМ&trade;News Reel Preview</title>
+	<style>
+	body
+	{
+		background-color: #222;
+		color: white;
+		padding: 32px;
+		margin: 0;
+	}
+	</style>
+</head>
+<body>
+	<span style='text-transform: uppercase; font-weight: 900;'>
+		Специальное предложение инвесторам&nbsp;&mdash;&nbsp;Опционы ЭКО-EARLYBIRD™:
+	</span>
+	<span>
+		Опционы ЭКО-EARLYBIRD™ позволяют приобрести ЭКО™ по курсу 1 ₽ = 1 ЭКО™ по истечении пятилетнего срока. 
+		С повышением покупательской способности и ликвидности опцион ЭКО-EARLYBIRD™ позволит инвестору выгодно приобрести 
+		ЭКО™ по курсу 1 ₽ и затем воспользоваться ЭКО™ для приобретения товаров и услуг в ЭКОНОМ™ по курсу соответствующему курсу ЕВРО, 
+		таким образом предоставляя инвестору доходность до 150% годовых. 
+		Опцион ЭКО-EARLYBIRD™ можно приобрести на бирже 
+		<a href='https://fx.ekonom.spb.ru/eko-earlybird'>ЭКОНОМ-FX™</a>
+	</span>
+</body>
+</html>
